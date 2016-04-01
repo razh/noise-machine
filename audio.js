ba874d55d549af3af1fe440d5cbe85d3e7443bd4
Raymond Zhou <somecrazyidiot@gmail.com>
2016-04-01 00:47:41 -0400

    Experiment with impulse responses

--- audio.js
+++ audio.js
@@ -1,12 +1,13 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
+
+export const audioContext = new AudioContext();
 const { sampleRate } = audioContext;
 
 // delay is in seconds.
-export function playSound( sound, delay ) {
+export function playSound( sound, delay, destination = audioContext.destination ) {
   const source = audioContext.createBufferSource();
   source.buffer = sound;
-  source.connect( audioContext.destination );
+  source.connect( destination );
   source.start( delay ? audioContext.currentTime + delay : 0 );
 }
 
@@ -17,7 +18,7 @@ export function generateAudioBuffer( fn, duration, volume ) {
   const buffer = audioContext.createBuffer( 1, length, sampleRate );
   const channel = buffer.getChannelData(0);
   for ( let i = 0; i < length; i++ ) {
-    channel[i] = fn( i / sampleRate, i / length ) * volume;
+    channel[i] = fn( i / sampleRate, i, channel ) * volume;
   }
 
   return buffer;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { generateAudioBuffer, playSound } from './audio';
+import { audioContext, generateAudioBuffer, playSound } from './audio';
 import { sin, saw, saw_i, tri, square, lowpass } from './osc';
 
 const A4 = 69;
@@ -21,7 +21,13 @@ const compress = ( fn, threshold, ratio ) => t => {
   }
 
   return out;
-}
+};
+
+const convolver = audioContext.createConvolver();
+convolver.connect( audioContext.destination );
+
+const impulseResponse = (t, i, a) => ( 2 * Math.random() - 1 ) * Math.pow( 1000, -i / a.length );
+convolver.buffer = generateAudioBuffer( impulseResponse, 1, 1 );
 
 const lowp_sin_delay = f => {
   const sawsin = mix( mix( saw( f / 4 ), sin( f ), 0.6 ), tri( f * 2 ), 0.1 );
@@ -33,11 +39,11 @@ const sound = generateAudioBuffer( lowp_sin_delay( note( 'a', 4 ) ), 0.3, 0.2 );
 const sound2 = generateAudioBuffer( lowp_sin_delay( note( 'a', 3 ) ), 0.3, 0.2 );
 const sound3 = generateAudioBuffer( lowp_sin_delay( note( 'e', 4 ) ), 0.3, 0.2 );
 
-playSound( sound );
-playSound( sound2, 0.2 );
-playSound( sound2, 0.4 );
-playSound( sound3, 0.6 );
-playSound( sound, 0.8 );
-playSound( sound2, 1 );
-playSound( sound3, 1.2 );
-playSound( sound2, 1.4 );
+playSound( sound, 0, convolver );
+playSound( sound2, 0.2, convolver );
+playSound( sound2, 0.4, convolver );
+playSound( sound3, 0.6, convolver );
+playSound( sound, 0.8, convolver );
+playSound( sound2, 1, convolver );
+playSound( sound3, 1.2, convolver );
+playSound( sound2, 1.4, convolver );
